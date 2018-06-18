a25066d28031682d016523d87df069a65344629a
Duong Nguyen-Huu <duongnhn@gmail.com>
2018-06-17 19:06:19 -0700

    add Nghe An to map

--- src/Constants.java
+++ src/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
 	static final int NUMBER_OF_RESOURCE_TYPES = 3;
 	static final int TOTAL_ENEMY_LIMIT = 3*NUMBER_OF_PLACES;
 	static final int NUMBER_OF_ENEMY_PER_TURN = 1;
-	static final int NUMBER_OF_TRIALS = 10;
+	static final int NUMBER_OF_TRIALS = 100;
+	
+	/* Only print log to console when set Debug to true */
 	static final boolean DEBUG = true;
 }
--- src/Map.java
+++ src/Map.java
@@ -1,8 +1,9 @@
 enum Name {
-	THANG_LONG,
+	HA_NOI,
 	HOA_BINH,
 	THAI_BINH,
 	THANH_HOA,
+	NGHE_AN,
 }
 
 public class Map {
@@ -10,16 +11,18 @@ public class Map {
 		
 	void init() {
 		places = new Place[Constants.NUMBER_OF_PLACES];
-		places[Name.THANG_LONG.ordinal()] = new Place("Thang Long", new Resource(1, 0, 0));
-		places[Name.HOA_BINH.ordinal()] = new Place("Hoa Binh", new Resource(0, 1, 0));
-		places[Name.THAI_BINH.ordinal()] = new Place("Thai Binh", new Resource(0, 0, 1));
-		places[Name.THANH_HOA.ordinal()] = new Place("Thanh Hoa", new Resource(0, 1, 0));
+		places[Name.HA_NOI.ordinal()] = new Place("Ha Noi", new Resource(0, 0, 1));//food
+		places[Name.HOA_BINH.ordinal()] = new Place("Hoa Binh", new Resource(0, 1, 0));//steel
+		places[Name.THAI_BINH.ordinal()] = new Place("Thai Binh", new Resource(0, 0, 1));//food
+		places[Name.THANH_HOA.ordinal()] = new Place("Thanh Hoa", new Resource(1, 0, 0));//wood
+		places[Name.NGHE_AN.ordinal()] = new Place("Nghe An", new Resource(1, 0, 0));//wood
 		
 		//link places
-		linkPlaces(Name.THANG_LONG.ordinal(), Name.HOA_BINH.ordinal());
-		linkPlaces(Name.THANG_LONG.ordinal(), Name.THAI_BINH.ordinal());
+		linkPlaces(Name.HA_NOI.ordinal(), Name.HOA_BINH.ordinal());
+		linkPlaces(Name.HA_NOI.ordinal(), Name.THAI_BINH.ordinal());
 		linkPlaces(Name.HOA_BINH.ordinal(), Name.THANH_HOA.ordinal());
 		linkPlaces(Name.THAI_BINH.ordinal(), Name.THANH_HOA.ordinal());
+		linkPlaces(Name.THANH_HOA.ordinal(), Name.NGHE_AN.ordinal());
 	}
 
 	public Place generateEnemy() {
